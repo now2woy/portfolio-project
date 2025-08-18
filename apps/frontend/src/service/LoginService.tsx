b525fd1697068a19a188c8619f52d6feb4159472
now2woy <now2woy@gmail.com>
2025-08-18 10:51:46 +0900

    FE-TSK-47 : 로그인 기능 수정

---
+++ apps/frontend/src/service/LoginService.tsx
@@ -0,0 +1,18 @@
+import { createApi } from '@/lib/api';
+import { ILoginProps } from "@/types/LoginType";
+import { authenticationProps } from '@/types/CommonType';
+
+/**
+ * API 기본 URL
+ */
+const BASE_SERVICE_URL = '/api/system/v1/auths';
+
+/**
+ * 게시글 입력
+ * @param params
+ * @returns 
+ */
+export const fetchLogin = async ( { authentication, data } : { authentication: authenticationProps, data: ILoginProps } ) => {
+    const api = createApi( authentication );
+    await api.post( `${ BASE_SERVICE_URL }/login`, data );
+};
